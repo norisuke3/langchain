346247a9da951d555b566b515b2a22079541f6ef
Noriaki Hamamoto <norisuke3@gmail.com>
2024-06-28 05:48:33 -0700

    added pirate-speak template to the pirate project

--- pirate/app/server.py
+++ pirate/app/server.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from langserve import add_routes
+from pirate_speak.chain import chain as pirate_speak_chain
 
 app = FastAPI()
 
@@ -9,9 +10,7 @@ app = FastAPI()
 async def redirect_root_to_docs():
     return RedirectResponse("/docs")
 
-
-# Edit this to add the chain you want to add
-add_routes(app, NotImplemented)
+add_routes(app, pirate_speak_chain, path="/pirate-speak")
 
 if __name__ == "__main__":
     import uvicorn
